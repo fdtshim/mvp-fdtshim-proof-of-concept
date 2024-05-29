19d32162e0df1ce1ff010988b0e6b386c56b82a2
Samuel Dionne-Riel <samuel@dionne-riel.com>
2024-05-29 15:00:15 -0400

    WIP

--- run.sh
+++ run.sh
@@ -10,14 +10,27 @@ ARGS=(
 	--boot "target/${RUST_TARGET}/debug/uefi-hello-world.efi"
 	--add-file "target/${RUST_TARGET}/debug/uefi-hello-world.efi:EFI/Boot/BootAA64.efi"
 	--qemu-path "$qemu"
-	--
+)
+QEMU_ARGS=(
 	#-serial stdio
 	-m 512M
 	-nographic # CTRL+A then X to quit
 )
+
 if [[ $RUST_TARGET == "aarch64-unknown-uefi" ]]; then
-	ARGS+=(-cpu cortex-a57)
-	ARGS+=(-M virt)
+	qemu-system-aarch64 -machine virt -cpu cortex-a57 -machine dumpdtb=test.dtb
+	ARGS+=(--add-file "test.dtb:EFI/Boot/virt.dtb")
+	QEMU_ARGS+=(-cpu cortex-a57)
+	QEMU_ARGS+=(-machine virt)
+	QEMU_ARGS+=(-dtb test.dtb)
 fi
+
+ARGS+=(
+	--
+	"${QEMU_ARGS[@]}"
+)
+
+PS4=" $ "
+set -x
 
 exec uefi-run "${ARGS[@]}"
