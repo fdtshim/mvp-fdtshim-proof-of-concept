53e199e20a9227aa85c9cf1b3a3ca0ada712c027
Samuel Dionne-Riel <samuel@dionne-riel.com>
2024-05-28 23:10:00 -0400

    WIP cross

--- pkgs.nix
+++ pkgs.nix
@@ -1,36 +1,101 @@
 { pkgs }:
 
+/*
+let pkgs' = pkgs; in # XXX
+let pkgs = pkgs'.pkgsCross.aarch64-multiplatform; in # XXX
+/* */
+
 let
+  inherit (pkgs)
+    lib
+    stdenv
+  ;
   pins = import ./npins;
   fenix = import pins.fenix {
-    inherit pkgs;
-    inherit (pkgs.stdenv) system;
+    pkgs = pkgs.buildPackages;
+    inherit (stdenv) system;
   };
-  overlays = pkgs.appendOverlays [(final: super: {
-  })];
+  rustTarget = {
+    "x86_64-linux" = "x86_64-unknown-uefi";
+    "aarch64-linux" = "aarch64-unknown-uefi";
+  }.${stdenv.system};
   rustToolchain = with fenix;
-    combine [
-      stable.rustc
-      stable.cargo
-      stable.clippy
-      stable.rustfmt
-      # FIXME: support more than x86_64
-      targets.x86_64-unknown-uefi.stable.rust-std
-      stable.rust-src
-    ]
+    combine (
+      [
+        stable.rustc
+        stable.cargo
+        stable.clippy
+        stable.rustfmt
+        stable.rust-src
+        targets.${rustTarget}.stable.rust-std
+      ]
+    )
   ;
+
+  overlays = pkgs.appendOverlays [(final: super: {
+      # XXX doesn't cross-compile on Nixpkgs :(
+      OVMF = {
+        fd = final.callPackage (
+          { runCommand
+          , stdenv
+          , fetchurl
+          }:
+          runCommand "OVMF-prebuilt" {
+            src = fetchurl {
+              url = "https://github.com/rust-osdev/ovmf-prebuilt/releases/download/edk2-stable202402-r1/edk2-stable202402-r1-bin.tar.xz";
+              hash = "sha256-kfMUjvFGeUJBx3gQpJz6PpJcg+tVxcyQ80cYzBsQ6es=";
+            };
+            arch =
+              if stdenv.hostPlatform.efiArch == "aa64"
+              then "aarch64"
+              else stdenv.hostPlatform.efiArch
+            ;
+          } ''
+            (
+            PS4=" $ " ; set -x
+            tar xf $src
+            cd */
+            mkdir -p $out
+            mv -t "$out" $arch/*
+            cd $out
+            if [[ "$arch" == "x64" ]]; then
+              cat vars.fd code.fd > OVMF.fd
+            else
+              cp code.fd OVMF.fd
+            fi
+            )
+          ''
+        ) {};
+      };
+  })];
 in
 {
+  inherit (overlays) OVMF;
+  inherit pkgs;
+  inherit stdenv;
   inherit (overlays) hello;
   inherit fenix;
-  shell = pkgs.mkShell {
-    RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/";
-    OVMF = pkgs.OVMF.fd;
+  shell = overlays.callPackage (
+    { mkShell
+    , rustToolchain
+    , rustTarget
+    , OVMF
+    , uefi-run
+    , qemu
+    }:
+    mkShell {
+      RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/";
+      OVMF = OVMF.fd;
+      RUST_TARGET = rustTarget;
 
-    buildInputs = with pkgs; [
-      rustToolchain
-      uefi-run
-      qemu
-    ];
+      depsBuildBuild = [
+        rustToolchain
+        uefi-run
+        qemu
+      ];
+    }
+  ) {
+    inherit rustToolchain;
+    inherit rustTarget;
   };
 }
--- run.sh
+++ run.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
+case "$RUST_TARGET" in
+	x86_64*) qemu=qemu-system-x86_64 ;;
+	aarch64*) qemu=qemu-system-aarch64 ;;
+esac
+
 ARGS=(
-	--bios-path="${OVMF}/FV/OVMF.fd"
-	--boot target/x86_64-unknown-uefi/debug/uefi-hello-world.efi
+	--bios-path="${OVMF}/OVMF.fd"
+	--boot "target/${RUST_TARGET}/debug/uefi-hello-world.efi"
+	--add-file "target/${RUST_TARGET}/debug/uefi-hello-world.efi:EFI/Boot/BootAA64.efi"
+	--qemu-path "$qemu"
 	--
-	-serial stdio
+	#-serial stdio
+	-m 512M
+	-nographic # CTRL+A then X to quit
 )
+if [[ $RUST_TARGET == "aarch64-unknown-uefi" ]]; then
+	ARGS+=(-cpu cortex-a57)
+	ARGS+=(-M virt)
+fi
 
 exec uefi-run "${ARGS[@]}"
