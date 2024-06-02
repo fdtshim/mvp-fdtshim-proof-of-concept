c3cb4520758e2eb4394c81ee302eba0747d12949
Samuel Dionne-Riel <samuel@dionne-riel.com>
2024-06-01 20:06:07 -0400

    WIP

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,9 @@ use crate::protocols::dt_fixup::{DtFixup, DtFixupFlags};
 extern crate alloc;
 extern crate flat_device_tree as fdt;
 use core::ffi::c_void;
+use log::debug;
 use log::info;
+use log::warn;
 use uefi::prelude::*;
 use uefi::table::boot::{MemoryType, SearchType};
 use uefi::Identify;
@@ -27,72 +29,74 @@ unsafe fn main(_image_handle: Handle, mut system_table: SystemTable<Boot>) -> St
 
     let boot_services = system_table.boot_services();
 
-    info!("");
-    info!("Reading mapping.dtb");
+    debug!("");
+    debug!("Reading mapping.dtb");
     let mapping_data =
         read_file(boot_services, path_for("mapping.dtb")).expect("Could not load mapping.dtb!!");
-    info!("mapping.dtb size: {}", mapping_data.len());
     let mapping_fdt = fdt::Fdt::from_ptr(mapping_data.as_ptr()).unwrap();
-    let dtb_path = try_matching(&system_table, &mapping_fdt)
-        .expect("Could not match device from ambiant data...");
+    match try_matching(&system_table, &mapping_fdt) {
+        Some(dtb_path) => {
+            let dtb = read_file(boot_services, path_for(dtb_path))
+                .expect("Could not load device-specific dtb!!");
 
-    let dtb =
-        read_file(boot_services, path_for(dtb_path)).expect("Could not load device-specific dtb!!");
+            let dt_fixup_handle = *boot_services
+                .locate_handle_buffer(SearchType::ByProtocol(&DtFixup::GUID))
+                .expect("EFI_DT_FIXUP_PROTOCOL is missing")
+                .first()
+                .unwrap();
+            let mut dt_fixup = boot_services
+                .open_protocol_exclusive::<DtFixup>(dt_fixup_handle)
+                .expect("EFI_DT_FIXUP_PROTOCOL could not be opened");
+            info!("Found EFI_DT_FIXUP_PROTOCOL!");
 
-    let dt_fixup_handle = *boot_services
-        .locate_handle_buffer(SearchType::ByProtocol(&DtFixup::GUID))
-        .expect("EFI_DT_FIXUP_PROTOCOL is missing")
-        .first()
-        .unwrap();
-    let mut dt_fixup = boot_services
-        .open_protocol_exclusive::<DtFixup>(dt_fixup_handle)
-        .expect("EFI_DT_FIXUP_PROTOCOL could not be opened");
-    info!("Found EFI_DT_FIXUP_PROTOCOL!");
+            let dtb_p = dtb.as_ptr() as *const c_void;
 
-    let dtb_p = dtb.as_ptr() as *const c_void;
+            let size = dtb.len();
+            info!("Loaded dtb binary size: {size}");
+            info!("    checking size required for fixups...");
+            // NOTE: We're technically applying the fixup here, too, but we only want the resulting `size` value.
+            match dt_fixup.fixup(dtb_p, &size, DtFixupFlags::DtApplyFixups) {
+                Ok(_) => {}
+                Err(status) => match status.status() {
+                    Status::BUFFER_TOO_SMALL => {}
+                    _ => {
+                        panic!("Error attempting to apply EFI_DT_FIXUP_PROTOCOL! {status}")
+                    }
+                },
+            };
+            info!("    => Required buffer size: {size}");
 
-    let size = dtb.len();
-    info!("Loaded dtb binary size: {size}");
-    info!("    checking size required for fixups...");
-    // NOTE: We're technically applying the fixup here, too, but we only want the resulting `size` value.
-    match dt_fixup.fixup(dtb_p, &size, DtFixupFlags::DtApplyFixups) {
-        Ok(_) => {}
-        Err(status) => match status.status() {
-            Status::BUFFER_TOO_SMALL => {}
-            _ => {
-                panic!("Error attempting to apply EFI_DT_FIXUP_PROTOCOL! {status}")
-            }
-        },
-    };
-    info!("    => Required buffer size: {size}");
+            let final_fdt = boot_services
+                .allocate_pool(MemoryType::ACPI_RECLAIM, size)
+                .expect("Failed to allocate ACPI_RECLAIM memory ({size} bytes) for final FDT");
+            let final_fdt_p = final_fdt as *const c_void;
 
-    let final_fdt = boot_services
-        .allocate_pool(MemoryType::ACPI_RECLAIM, size)
-        .expect("Failed to allocate ACPI_RECLAIM memory ({size} bytes) for final FDT");
-    let final_fdt_p = final_fdt as *const c_void;
+            final_fdt.copy_from(dtb.as_ptr(), dtb.len());
 
-    final_fdt.copy_from(dtb.as_ptr(), dtb.len());
+            info!("Applying DT Fixups to new and final FDT");
+            match dt_fixup.fixup(
+                final_fdt_p,
+                &size,
+                DtFixupFlags::DtApplyFixups | DtFixupFlags::DtReserveMemory,
+            ) {
+                Ok(_) => {
+                    info!("Succesfully applied fixups.")
+                }
+                Err(status) => {
+                    panic!("Error! {status}")
+                }
+            };
 
-    info!("Applying DT Fixups to new and final FDT");
-    match dt_fixup.fixup(
-        final_fdt_p,
-        &size,
-        DtFixupFlags::DtApplyFixups | DtFixupFlags::DtReserveMemory,
-    ) {
-        Ok(_) => {
-            info!("Succesfully applied fixups.")
+            install_efi_dtb_table(&system_table, final_fdt_p)
+                .expect("Failed to install updated EFI_DT_TABLE!");
         }
-        Err(status) => {
-            panic!("Error! {status}")
+        None => {
+            warn!("No DTB could be matched from ambiant data. (This may not be a problem.)");
         }
     };
 
-    install_efi_dtb_table(&system_table, final_fdt_p)
-        .expect("Failed to install updated EFI_DT_TABLE!");
-
-    info!("");
-    info!("NOTE: successfully ran to the end.");
-    info!("Staling for 10s.");
+    info!("NOTE: fdtshim.efi ran likely successfully to the end.");
+    info!("Stalling for 10s.");
     boot_services.stall(10_000_000);
     uefi::allocator::exit_boot_services();
     Status::SUCCESS
--- src/matching.rs
+++ src/matching.rs
@@ -4,9 +4,9 @@ use flat_device_tree::Fdt;
 use log::debug;
 use log::info;
 use uefi::prelude::*;
-use uefi::Result;
+//use uefi::Result;
 
-pub unsafe fn try_matching<'a>(st: &SystemTable<Boot>, mapping_fdt: &'a Fdt) -> Result<&'a str> {
+pub unsafe fn try_matching<'a>(st: &SystemTable<Boot>, mapping_fdt: &'a Fdt) -> Option<&'a str> {
     debug!("-> Attempting to match device from ambiant data...");
 
     let ambiant_fdt = fdt::Fdt::from_ptr(get_efi_dtb_table(&st) as *const u8).unwrap();
@@ -14,13 +14,14 @@ pub unsafe fn try_matching<'a>(st: &SystemTable<Boot>, mapping_fdt: &'a Fdt) -> 
     let compatible = ambiant_fdt.root().expect("").compatible();
     let compatibles: Vec<&str> = compatible.all().collect();
 
-    let matched_by_fdt = mapping_fdt
-        .find_compatible(&compatibles)
-        .expect("Compatible not found");
+    match mapping_fdt.find_compatible(&compatibles) {
+        Some(matched_by_fdt) => {
+            let dtb_path = matched_by_fdt.property("dtb").unwrap().as_str().unwrap();
+            info!("Found a `compatible`-based match:");
+            info!("    This device matches DTB path: {}", dtb_path);
 
-    let dtb_path = matched_by_fdt.property("dtb").unwrap().as_str().unwrap();
-    info!("Found a `compatible`-based match:");
-    info!("    This device matches DTB path: {}", dtb_path);
-
-    Ok(dtb_path)
+            Some(dtb_path)
+        }
+        None => None,
+    }
 }
